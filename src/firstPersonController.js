8038305e967ed2d47c9c8fb25e1f1d1b9413cb5a
peterbono <63565173+peterbono@users.noreply.github.com>
2021-02-04 00:24:35 +0100

    Update with esd 3D scene

--- src/firstPersonController.js
+++ src/firstPersonController.js
@@ -1,11 +1,12 @@
 import './style.css'
 import * as THREE from 'three'
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as dat from 'dat.gui'
 
 let camera, scene, renderer, controls;
 
 const objects = [];
-
 let raycaster;
 
 let moveForward = false;
@@ -27,7 +28,7 @@ function init() {
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xffffff );
-    scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+    scene.fog = new THREE.Fog( 0xffffff, 50, 1000 );
 
     const light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
     light.position.set( 0.5, 1, 0.75 );
@@ -85,7 +86,7 @@ function init() {
                 break;
 
             case 'Space':
-                if ( canJump === true ) velocity.y += 350;
+                if ( canJump === true ) velocity.y += 300;
                 canJump = false;
                 break;
 
@@ -126,6 +127,22 @@ function init() {
 
     raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 
+    
+     // Models
+     const gltfLoader = new GLTFLoader()
+     gltfLoader.load(
+         '/models/structure.glb',
+         (gltf) => {
+             console.log(gltf)
+             gltf.scene.scale.set(30, 30, 30);
+             const box = new THREE.Box3().setFromObject(gltf.scene);
+             const center = box.getCenter(new THREE.Vector3());
+             gltf.scene.position.x += (gltf.scene.position.x - center.x);
+             gltf.scene.position.y += (gltf.scene.position.y - center.y);
+             gltf.scene.position.z += (gltf.scene.position.z - center.z);
+             scene.add(gltf.scene)
+         }
+     )
     // floor
 
     let floorGeometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
@@ -135,32 +152,32 @@ function init() {
 
     let position = floorGeometry.attributes.position;
 
-    for ( let i = 0, l = position.count; i < l; i ++ ) {
+    // for ( let i = 0, l = position.count; i < l; i ++ ) {
+    //     //Texture sol
+    //     vertex.fromBufferAttribute( position, i );
 
-        vertex.fromBufferAttribute( position, i );
+    //     vertex.x += Math.random() * 20 - 10;
+    //     vertex.y += Math.random() * 1;
+    //     vertex.z += Math.random() * 20 - 10;
 
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
-        vertex.z += Math.random() * 20 - 10;
+    //     position.setXYZ( i, vertex.x, vertex.y, vertex.z );
 
-        position.setXYZ( i, vertex.x, vertex.y, vertex.z );
-
-    }
+    // }
 
     floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
 
     position = floorGeometry.attributes.position;
     const colorsFloor = [];
 
-    for ( let i = 0, l = position.count; i < l; i ++ ) {
-
+/*     for ( let i = 0, l = position.count; i < l; i ++ ) {
+        //Contrôle couleur des sols
         color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
         colorsFloor.push( color.r, color.g, color.b );
 
     }
 
     floorGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsFloor, 3 ) );
-
+*/
     const floorMaterial = new THREE.MeshBasicMaterial( { vertexColors: true } );
 
     const floor = new THREE.Mesh( floorGeometry, floorMaterial );
@@ -168,34 +185,34 @@ function init() {
 
     // objects
 
-    const boxGeometry = new THREE.BoxGeometry( 20, 20, 20 ).toNonIndexed();
+    // const boxGeometry = new THREE.BoxGeometry( 20, 20, 20 ).toNonIndexed();
 
-    position = boxGeometry.attributes.position;
-    const colorsBox = [];
+    // position = boxGeometry.attributes.position;
+    // const colorsBox = [];
 
-    for ( let i = 0, l = position.count; i < l; i ++ ) {
+    // for ( let i = 0, l = position.count; i < l; i ++ ) {
 
-        color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        colorsBox.push( color.r, color.g, color.b );
+    //     color.setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    //     colorsBox.push( color.r, color.g, color.b );
 
-    }
+    // }
 
-    boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
+    // boxGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colorsBox, 3 ) );
 
-    for ( let i = 0; i < 500; i ++ ) {
+    // for ( let i = 0; i < 500; i ++ ) {
 
-        const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
-        boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    //     const boxMaterial = new THREE.MeshPhongMaterial( { specular: 0xffffff, flatShading: true, vertexColors: true } );
+    //     boxMaterial.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
 
-        const box = new THREE.Mesh( boxGeometry, boxMaterial );
-        box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-        box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
-        box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+    //     const box = new THREE.Mesh( boxGeometry, boxMaterial );
+    //     box.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+    //     box.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+    //     box.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
 
-        scene.add( box );
-        objects.push( box );
+    //     scene.add( box );
+    //     objects.push( box );
 
-    }
+    // }
 
     //
 
@@ -236,8 +253,8 @@ function animate() {
 
         const delta = ( time - prevTime ) / 1000;
 
-        velocity.x -= velocity.x * 10.0 * delta;
-        velocity.z -= velocity.z * 10.0 * delta;
+        velocity.x -= velocity.x * 5.0 * delta;
+        velocity.z -= velocity.z * 5.0 * delta;
 
         velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
 
@@ -245,8 +262,8 @@ function animate() {
         direction.x = Number( moveRight ) - Number( moveLeft );
         direction.normalize(); // this ensures consistent movements in all directions
 
-        if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
-        if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
+        if ( moveForward || moveBackward ) velocity.z -= direction.z * 300.0 * delta;
+        if ( moveLeft || moveRight ) velocity.x -= direction.x * 300.0 * delta;
 
         if ( onObject === true ) {
 
