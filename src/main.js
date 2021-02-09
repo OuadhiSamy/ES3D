8dcc127aee5d3091c84f3d89ad233cf6d68f74a7
peterbono <63565173+peterbono@users.noreply.github.com>
2021-02-09 14:43:42 +0100

    play animation on interaction

--- src/main.js
+++ src/main.js
@@ -116,6 +116,20 @@ var material = new THREE.MeshPhongMaterial({
 	opacity: 1,
 	transparent: true
 });
+
+let mixer = null
+
+var action;
+// load chair
+gltfLoader.load( 'chaise.glb', ( gltf ) => {
+	gltf.scene.scale.set(1,1,1)
+	scene.add( gltf.scene );
+	action = mixer.clipAction(gltf.animations[0])
+	action.setLoop( THREE.LoopOnce )
+})
+
+
+
 gltfLoader.load('threejs_colliders.glb', (gltf) => {
 
 	scene.add(gltf.scene);
@@ -138,6 +152,7 @@ gltfLoader.load('threejs_colliders.glb', (gltf) => {
 
 	});
 
+	console.log(scene)
 
 	animate();
 
@@ -149,6 +164,8 @@ gltfLoader.load('threejs_colliders.glb', (gltf) => {
 const clock = new THREE.Clock();
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x88ccff);
+
+mixer = new THREE.AnimationMixer(scene)
 
 
 const box1 = new THREE.Mesh(
@@ -204,35 +221,6 @@ function updatePositionHelper() {
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.rotation.order = 'YXZ';
 camera.rotateY(- Math.PI * 0.5);
-
-/**
- * Lights
- */
-// const ambientlight = new THREE.AmbientLight(0x6688cc);
-// scene.add(ambientlight);
-
-// const fillLight1 = new THREE.DirectionalLight(0xff9999, 0.5);
-// fillLight1.position.set(- 1, 1, 2);
-// scene.add(fillLight1);
-
-// const fillLight2 = new THREE.DirectionalLight(0x8888ff, 0.2);
-// fillLight2.position.set(0, - 1, 0);
-// scene.add(fillLight2);
-
-// const directionalLight = new THREE.DirectionalLight(0xffffaa, 1.2);
-// directionalLight.position.set(- 5, 25, - 1);
-// directionalLight.castShadow = true;
-// directionalLight.shadow.camera.near = 0.01;
-// directionalLight.shadow.camera.far = 500;
-// directionalLight.shadow.camera.right = 30;
-// directionalLight.shadow.camera.left = - 30;
-// directionalLight.shadow.camera.top = 30;
-// directionalLight.shadow.camera.bottom = - 30;
-// directionalLight.shadow.mapSize.width = 1024;
-// directionalLight.shadow.mapSize.height = 1024;
-// directionalLight.shadow.radius = 4;
-// directionalLight.shadow.bias = - 0.00006;
-// scene.add(directionalLight);
 
 var ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
 var lightFront = new THREE.SpotLight(0xFFFFFF, 2.04, 100);
@@ -565,6 +553,9 @@ function controls(deltaTime) {
 					// Prevent from animate more than once
 					updateInsightState(true)
 					isAnimationInProgress = true
+					////play action chair
+					action.stop();
+					action.play();
 
 					// // Hide Arrows
 					// for (const arrow of arrows) {
@@ -676,9 +667,16 @@ function animate() {
 	if (closestObject && !isAnimationInProgress && isPlayerLookingAtObject()) {
 		updateInteractionButtonState(true)
 		canAnimate = true;
+
 	} else {
 		canAnimate = false
 		updateInteractionButtonState(false)
+	}
+
+	//Calcul mixer
+	if( mixer !== null)
+	{
+		mixer.update(deltaTime)
 	}
 
 	controls(deltaTime);
